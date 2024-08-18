21cc732b7c891749ac34f02c9794cff2745eb189
Manprabesh <billionaireboruah31@gmail.com>
2024-08-18 19:47:44 +0530

    user mmodel changed

--- model/user.moodel.js
+++ model/user.moodel.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
 try {
-    // const mongoose = require('mongoose');
-
-    // const uri = 'mongodb+srv://monboruah0986:4QE5J5pqmn5r6YxD@project0.jsyk0.mongodb.net/mydatabase?retryWrites=true&w=majority';
-
-    // mongoose.connect(uri)
-    //     .then(() => console.log('MongoDB connected via Mongoose'))
-    //     .catch(err => console.error('MongoDB connection error:', err));
-
-
     const userSchema = new mongoose.Schema({
-        name: String
+        name:{
+            type:String,
+            required:true,
+        },
+        email:{
+            type:String,
+            required:true,
+        },
+        password:{
+            type:String,
+            required:true,
+        },
     });
 
     module.exports = mongoose.model('user', userSchema);
